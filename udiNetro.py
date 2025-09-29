b6412103024542f98e7b16d6cf12b44ad0dec277
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2025-09-29 11:23:02 -0700

    Update udiNetro.py

--- udiNetro.py
+++ udiNetro.py
@@ -90,7 +90,7 @@ class netroStart(udi_interface.Node):
         logging.debug(f'Instanciating nodes for {self.serialID_list}')
         for indx, serial_nbr in enumerate(self.serialID_list):
             logging.debug(f'Instanciating nodes for {serial_nbr}')
-            #access  = netroAccess.netroType(serial_nbr)
+
             dev_type, name = netroAccess.netroType(serial_nbr)
             logging.debug(f'Name : {name}, {dev_type }')
             if dev_type == 'controller':
