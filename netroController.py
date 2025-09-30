b54e18e65af35edf8fa51093f80f0671c3920160
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2025-09-29 17:18:29 -0700

    Update netroController.py

--- netroController.py
+++ netroController.py
@@ -80,12 +80,13 @@ class netroController(udi_interface.Node):
         
 
     def start(self):                
-        logging.debug('Start Netro Irrigation Node')  
+        logging.debug('Start Netro Irrigation Node {self.serial_id}')  
 
         while not self.config_done:
             time.sleep(1)
             logging.info(f'Waiting for system to initialize {self.customParam_done} {self.config_done}')
         #self.CO_setDriver('ST', 1)
+
         self.netro_api = netroAccess(self.serial_id, self.EVENTDAYS, self.MOIST_DAYS, self.SCH_DAYS)
         self.zone_nodes = {}
         zone_addresses = [self.primary]
