3d86a9a362c39516b4b096e1701669dfdba49b06
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2025-10-03 19:18:38 -0700

    Update netroZone.py

--- netroZone.py
+++ netroZone.py
@@ -69,7 +69,7 @@ class netroZone(udi_interface.Node):
             self.CO_setDriver('GV1',self.zoneconfig2ISY(self.netro_api.get_zone_info(self.zone_nbr, 'smart'))) 
             self.CO_setDriver('GV2', self.netro_api.moisture(self.zone_nbr), 70)
             self.CO_setDriver('GV3', self.netro_api.moisture_slope(self.zone_nbr),70 )
-
+            logging.debug(f"Zone {self.zone_nbr} status {self.netro_api.get_zone_info(self.zone_nbr, 'status')}" )
             if self.netro_api.zone_config(self.zone_nbr, 'smart' ) in ['ASSISTANT', 'TIMER', 'SMART']: #why not smart?
 
                 self.CO_setDriver('GV4', self.netro_api.get_zone_info(self.zone_nbr, 'last_start') , 151)
